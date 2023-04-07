bf37018e7aa9e6416759a5de0ee4c371c4287f52
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-04-07 08:09:50 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,29 @@
-#!/usr/bin/env python3
-"""
-Fabric script to distribute an archive to web servers
-"""
-
-import os
-from fabric.api import env, run, put
+#!/usr/bin/python3
+"""web server distribution"""
+from fabric.api import *
+import os.path
 
 env.user = 'ubuntu'
 env.hosts = ["100.25.193.56", "18.210.19.239"]
+env.key_filename = "~/id_rsa"
+
 
 def do_deploy(archive_path):
-    # Check if archive_path exists
-    if not os.path.exists(archive_path):
-        print("File does not exist!")
+    """distributes an archive to your web servers
+    """
+    if os.path.exists(archive_path) is False:
         return False
-
-    # Extract archive filename without extension
-    filename = os.path.basename(archive_path)
-    filename_no_ext = os.path.splitext(filename)[0]
-
-    # Upload archive to /tmp/ directory on web server
-    put(archive_path, "/tmp/")
-
-    # Create directory to uncompress archive
-    run("sudo mkdir -p /data/web_static/releases/{}/".format(filename_no_ext))
-
-    # Uncompress archive to directory
-    run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename, filename_no_ext))
-
-    # Remove archive from web server
-    run("sudo rm /tmp/{}".format(filename))
-
-    # Delete symbolic link /data/web_static/current
-    run("sudo rm -rf /data/web_static/current")
-
-    # Create new symbolic link to new version of code
-    run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename_no_ext))
-
-    # Return True if all operations were successful
-    return True
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        return True
+    except:
+        return False
