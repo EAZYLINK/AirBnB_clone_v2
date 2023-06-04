cb57016d46d3a5b67e915f887dafe7364fb5d3c2
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-06-04 12:27:27 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from fabric.state import commands, connections
 import os.path
 
 env.user = 'ubuntu'
-env.hosts = ["100.25.193.56", "18.210.19.239"]
+env.hosts = ["100.24.237.31", "100.25.129.126"]
 env.key_filename = "~/id_rsa"
 
 
